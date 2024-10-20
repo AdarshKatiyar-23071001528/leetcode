db311ba990a314c7e9959d7b48bb420b872d8754
Adarsh katiyar <adarshkatiyar73@gmail.com>
2024-10-20 13:26:25 +0530

    Time: 16 ms (98.67%), Space: 17 MB (59.09%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,25 @@
+def removeSpecial(s):
+    chars = "!@#$%^&*()-+?_=,<>/;."
+    for char in chars:
+        s3 = s.replace(char, "")
+    return s3
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s1=''
+        s2 = removeSpecial(s)
+        s2 =s2.lower()
+        for i in s2:
+            # if 65<=ord(i) and 90>=ord(i):
+            #     n = ord(i)+32
+            #     s1+=chr(n)
+            if len(s2)==2:
+                if s2[0]==s2[1]:
+                    return True
+                else:
+                    return False
+            elif 48<=ord(i) and 57>=ord(i):
+                s1+=i
+            elif 97<=ord(i) and 122>=ord(i):
+                s1+=i
+        r = s1[::-1]
+        return(r==s1)
